2c0688faf59839610129d9621252b79fa63a636f
adam-poulton <adam.poulton5@gmail.com>
2019-11-29 21:45:55 +1100

    Add sound channels

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 /*
   Based on the tutorial by Wes Bos https://www.youtube.com/watch?v=VuN8qwZoego
 */
+class Channel {
+  audio;
+  resource;
+  constructor(audio){
+    this.audio = audio;
+    this.resource = new Audio(audio.src)
+  }
+  play(){
+    this.resource.play()
+  }
+}
 
+class Switcher {
+  channels = [];
+  num;
+  index;
+  constructor(audio, num){
+    this.num = num;
+    this.index = 0;
+    for (let i = 0; i < num; i++){
+      this.channels.push(new Channel(audio))
+    }
+  }
+  play(){
+    this.channels[this.index++].play();
+    this.index = this.index < this.num ? this.index : 0;
+  }
+}
 
 function playSound(e){
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  const audio = sounds.find(({key}) => key == e.keyCode)
   if (!audio) return; // stop the function
-  audio.currentTIme = 0; // rewind to the start
-  audio.play();
+  audio.sound.play();
   key.classList.add('playing');
 };
 
@@ -17,6 +43,14 @@ function removeTransition(e){
   this.classList.remove('playing');
 };
 
+
+const sounds = [];
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => {
+    const keyCode = parseInt(key.getAttribute('data-key'));
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    key.addEventListener('transitionend', removeTransition)
+    sounds.push({key: keyCode, sound: new Switcher(audio, 5)})
+    });
+console.log(sounds)
 window.addEventListener('keydown', playSound);
