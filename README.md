1e66f181dec71ac5edef137491c35ad2d1e142fd
adam-poulton <adam.poulton5@gmail.com>
2019-11-29 20:48:23 +1100

    Complete proof of concept

---
+++ README.md
@@ -0,0 +1,3 @@
+# JavaScript Drums
+
+Based on the tutorial by Wes Bos https://www.youtube.com/watch?v=VuN8qwZoego
---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+  Based on the tutorial by Wes Bos https://www.youtube.com/watch?v=VuN8qwZoego
+*/
+
+
+function playSound(e){
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if (!audio) return; // stop the function
+  audio.currentTIme = 0; // rewind to the start
+  audio.play();
+  key.classList.add('playing');
+};
+
+function removeTransition(e){
+  if(e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+};
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
